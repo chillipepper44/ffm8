55d018d7a87a9d4af820cbd08f7774f15c66f646
chillipepper44 <prikthaipo@gmail.com>
2025-05-26 21:58:37 +0700

    Update

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ def index():
             result = parse_manifest_to_ffm8(file)
     return render_template('index.html', output=result)
 
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
