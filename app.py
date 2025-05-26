4e4d0e6fd668895e8161ee6e91254baafc95092b
chillipepper44 <prikthaipo@gmail.com>
2025-05-26 23:27:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from ffm_parser import parse_manifest_to_ffm8
+import os
 
 app = Flask(__name__)
 
@@ -12,5 +13,7 @@ def index():
             output = parse_manifest_to_ffm8(file)
     return render_template("index.html", output=output)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
