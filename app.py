74c96c7df4ac29c87ae572cbf576d561b5190365
chillipepper44 <prikthaipo@gmail.com>
2025-05-29 03:57:28 +0700

    Update

--- app.py
+++ app.py
@@ -88,5 +88,8 @@ def index():
             result_text = parse_pdf_to_ffm8(file)
     return render_template("index.html", output=result_text)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
